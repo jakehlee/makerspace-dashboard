40f7732cc69795072e87bc728f2af9a119659681
Jake Lee <jakehlee1@gmail.com>
2020-01-22 00:37:54 -0500

    First draft

---
+++ main.js
@@ -0,0 +1,65 @@
+apikey = 'redacted'
+
+function start() {
+	gapi.client.init({
+		'apikey': apikey,
+	}).then(function() {
+		var timestamp = (new Date()).toISOString();
+		return gapi.client.request({
+			'path': 'https://www.googleapis.com/calendar/v3/calendars/nod789fge1kv6i6ip1b000pv8k%40group.calendar.google.com/events?singleEvents=True&timeMin='+timestamp+'&orderBy=startTime'+'&key='+apikey
+		});
+	}).then(function(response) {
+		var respjson = response.result;
+		var now = Date.now();
+
+		var currTime = new Date()
+		var startTime = new Date(currTime.getTime());
+		startTime.setHours('10');
+		startTime.setMinutes('00');
+		startTime.setSeconds('00');
+
+		var endTime = new Date(currTime.getTime());
+		endTime.setHours('18');
+		endTime.setMinutes('00');
+		endTime.setSeconds('00');
+
+		var billEventName = respjson['items'][0]['summary'];
+		if (billEventName == "Open 10 AM-6 PM by Bill Miller" && currTime >= startTime && currTime <= endTime) {
+			document.getElementById("billHere").innerHTML = "IN";
+			document.getElementById("billHere").classList.add('badge-success');
+		} else {
+			document.getElementById("billHere").innerHTML = "OUT";
+			document.getElementById("billHere").classList.add('badge-danger');
+		}
+
+
+		for(i=1; i < respjson['items'].length; i++) {
+			var eventName = respjson['items'][i]['summary'];
+			var eventStart = Date.parse(respjson['items'][i]['start']['dateTime']);
+			var eventEnd = Date.parse(respjson['items'][i]['end']['dateTime']);
+			if(eventEnd - eventStart > (12*60*60*1000)) {
+				// event is longer than 12 hours, skip
+				continue;
+			} else if (now > eventStart && now < eventEnd) {
+				document.getElementById("currEvent").innerHTML = eventName;
+				break;
+			} else {
+				document.getElementById("currEvent").innerHTML = "N/A";
+				break;
+			}
+		}
+
+	}, function(reason) {
+		console.log("ERROR: " + reason.result.error.message);
+	});
+};
+
+gapi.load('client', start);
+
+var calCheck = setInterval(function() {
+	gapi.load('client', start);
+}, 15*60*1000);
+
+setTimeout(function() {
+	location.reload();
+}, 24*60*60*1000);
